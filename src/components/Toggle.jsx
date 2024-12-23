76cc5531ecbd7d664c188c53002b82dadf748686
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2024-12-23 16:29:10 +0600

    fixed the toggle components border

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -20,14 +20,14 @@ const Toggle = (props) => {
     <div className="relative flex w-fit items-center rounded-full">
       <input
         type="checkbox"
-        className="toggle toggle-accent"
+        className="toggle toggle-accent rounded-full"
         onChange={(e) => props.checkCheckBox(e)}
         defaultChecked
       />
       <motion.span
         layout
         transition={{ type: 'spring', damping: 15, stiffness: 250 }}
-        className="h-full w-1/2 rounded-full bg-gradient-to-r from-violet-600 to-indigo-600"
+        className="h-full w-1/2 rounded-full bg-gradient-to-r from-violet-600 to-indigo-600 "
       />
     </div>
   );
