80faaff4a88c0f60937ebbaa48062647d599bd44
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2024-12-29 23:22:47 +0600

    added new hero section called VideoHero.jsx for auto play a video

--- src/components/VideoHero.jsx
+++ src/components/VideoHero.jsx
@@ -1,5 +1,18 @@
-const VideoHeader = () => {
-  return <div>VideoHeader</div>;
+const VideoHero = () => {
+  return (
+    <section className="w-full h-full">
+      <video autoPlay muted loop className="min-w-full min-h-full">
+        <source
+          src="./assets/videos/videohero/videohero.mp4"
+          type="video/mp4"
+        />
+        <source
+          src="./assets/videos/videohero/videohero.webm"
+          type="video/webm"
+        />
+      </video>
+    </section>
+  );
 };
 
-export default VideoHeader;
+export default VideoHero;
