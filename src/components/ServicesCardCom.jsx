0df61d8ed740fa35b96ce862e243a49e5c624c89
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-01-06 10:41:14 +0600

    fixed the mobile view of services card components

--- src/components/ServicesCardCom.jsx
+++ src/components/ServicesCardCom.jsx
@@ -2,7 +2,7 @@ const ServicesCardCom = ({ src, children }) => {
   return (
     <>
       <div className="w-[550px] flex justify-center items-center md:items-start">
-        <img src={src} alt={src} className="w-3/4 md:4/5" />
+        <img src={src} alt={src} className="w-1/2 md:4/5" />
       </div>
       <ul className="flex flex-col justify-center mt-8 text-sm m-auto w-96 md:text-lg md:w-full">
         {children}
