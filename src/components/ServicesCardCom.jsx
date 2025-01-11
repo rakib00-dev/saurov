f15e8fecd8cf46623cc94e0a82069e0dca453b19
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-01-11 13:04:39 +0600

    updated the mobile screen view

--- src/components/ServicesCardCom.jsx
+++ src/components/ServicesCardCom.jsx
@@ -4,7 +4,7 @@ const ServicesCardCom = ({ src, children }) => {
       <div className="w-[550px] flex justify-center items-center md:items-start">
         <img src={src} alt={src} className="w-1/2 md:4/5" />
       </div>
-      <ul className="flex flex-col justify-center mt-8 text-sm m-auto w-96 md:text-lg md:w-full">
+      <ul className="flex flex-col justify-center items-start mt-8 text-[.5rem] m-auto w-fit md:text-lg md:w-full">
         {children}
       </ul>
     </>
